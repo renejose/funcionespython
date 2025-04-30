37fd97f224206d1e5a34f18ae6c6645c80ce4a62
renejose <ingenieroshardware@gmail.com>
2025-04-29 20:43:03 -0500

    nuevos cambios

---
+++ estructurasDatos/DiccionariosyConjuntos.py
@@ -0,0 +1,36 @@
+# ejemplos de conjuntos 
+# persona = {
+#     "nombre": "Anita",
+#     "edad" : 29,
+#     "ciudad" : "Lima"
+# }
+# print("======================== impresiones =========================")
+# #print(persona["nombre"])
+
+# persona["profesion"] = "Ingeniera Industrial"
+# #print(persona)
+
+# # for clave,valor in persona.items():
+# #     print(clave," : ", valor)
+
+
+# ejemplos de conjuntos 
+print("======================== impresiones =========================")
+numeros_1 = {1,2,3,4,5,6}
+
+numeros_2 = {4,5,6,7,8,9,10}
+
+union = numeros_1 | numeros_2
+
+
+interseccion  = numeros_1 & numeros_2
+
+print(interseccion)
+
+#agregar un elemento
+# numeros_1.add(15)
+
+# #eliminar elemento
+# numeros_1.discard(4)
+
+
---
+++ estructurasDatos/estructura_ejemplos.py
@@ -0,0 +1,54 @@
+#print("hola a todos ")
+# estructura de datos dinamicas 
+# Listas
+# Tuplas
+# Conjuntos 
+# Diccionarios 
+
+'''
+nombres = ["Alberto","Alejandro","Alejandro","Angel","Alicia","Brayan","Carla"]
+for x in nombres:
+    print(x)
+'''
+print("================= Antes de la Ejecucion  =================")
+
+datos = [15,13.9,True,False,"Maria del Rosario",14,15,13.9,True,False,"Maria del Rosario",14,15,13.9,True,False,"Maria del Rosario",14,15,13.9,True,False,"Maria del Rosario",14,"oso polar"]
+
+# print(datos[1])
+
+# print(datos[4])
+
+print("=======================================")
+
+cantidadElementos = len(datos)
+
+print("la cantidad nueva es: ", cantidadElementos)
+
+#dato = datos[cantidadElementos-1]
+
+print("=======================================")
+
+datos.remove("Maria del Rosario")
+
+cantidadElementosNuevos = len(datos)
+
+print("la cantidad nueva es: ", cantidadElementosNuevos)
+
+# datos.insert(5+30,"Francisco")
+# #datos.append("Francisco")
+
+# for y in datos:
+#     print(y)
+
+
+# datostupla = (15,13.9,True,False,"Maria del Rosario",14)
+# datostupla.append("Francisco")
+
+
+
+
+
+
+
+
+
